fa315e4fdd805bb86bf1d8b72716120079ee0909
Noor Fatima <devnoorfatima@gmail.com>
2019-10-16 17:26:58 +0500

    Made navbar

---
+++ portfolio-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from "./Components/Navbar";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Navigation />
+    </div>
+  );
+}
+
+export default App;
